db9159dc8e2521e70fc4e572deb12145a6c42bf6
Khang <letankhang2@gmail.com>
2018-03-27 23:58:57 +0700

    end

--- src/main/java/bootsample/model/Interview_round.java
+++ src/main/java/bootsample/model/Interview_round.java
@@ -1,24 +1,18 @@
 package bootsample.model;
 
-<<<<<<< HEAD
 import java.util.Set;
 
-=======
 import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
->>>>>>> 8e040568b8b0133009c99f696a0ba9516875df83
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-<<<<<<< HEAD
 import javax.persistence.ManyToMany;
-=======
 import javax.persistence.OneToMany;
->>>>>>> 8e040568b8b0133009c99f696a0ba9516875df83
 
 @Entity(name = "interview_round")
 public class Interview_round {
@@ -28,16 +22,12 @@ public class Interview_round {
 	private String roundname;
 	private Integer guideline_id;
 	private String description;
-<<<<<<< HEAD
 	
 	@ManyToMany(mappedBy = "rounds")
 	private Set<Interview_process> processes;
 	
 	public Interview_round() {}
 	
-	public Interview_round(int no, String roundname, int guideline_id, String description) {
-=======
-
 	// thuộc tính fectch là LAZY
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "interview_round", cascade = CascadeType.ALL)
 	private Set<InterviewResult> interviewresult = new HashSet<InterviewResult>(0);
@@ -50,20 +40,15 @@ public class Interview_round {
 		this.interviewresult = interviewresult;
 	}
 
-	public Interview_round() {
-	}
-
 	public Interview_round(int no, String roundname, Integer guideline_id, String description,
 			Set<InterviewResult> interviewresult) {
 		super();
->>>>>>> 8e040568b8b0133009c99f696a0ba9516875df83
 		this.no = no;
 		this.roundname = roundname;
 		this.guideline_id = guideline_id;
 		this.description = description;
 		this.interviewresult = interviewresult;
 	}
-<<<<<<< HEAD
 	
 	public Set<Interview_process> getProcesses() {
 		return processes;
@@ -73,9 +58,6 @@ public class Interview_round {
 		this.processes = processes;
 	}
 	
-=======
-
->>>>>>> 8e040568b8b0133009c99f696a0ba9516875df83
 	public int getNo() {
 		return no;
 	}
